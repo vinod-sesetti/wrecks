cfd4d2a6ecfb1d070ef47e493f40edc579791e5b
manikantan-nyros <manikantan_nyros@yahoo.com>
2015-12-01 12:27:38 +0530

    adding files2

--- app_test.py
+++ app_test.py
@@ -9,7 +9,7 @@ class CitiesTestCase(unittest.TestCase):
     tester = app.test_client(self)
     response = tester.get('/cities.json', content_type='application/json')
     self.assertEqual(response.status_code, 200)
-    self.assertEqual(response.data, json.dumps(['Amsterdam', 'San Francisco', 'Berlin', 'New York','kakinada','hyderabd']))
+    self.assertEqual(response.data, json.dumps(['Amsterdam', 'San Francisco', 'Berlin', 'New York','kakinada','hyderabd','chennai','kolkata']))
 
 if __name__ == '__main__':
     unittest.main()
